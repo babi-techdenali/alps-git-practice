4ad7315b5c8af718d8da04ab05ba3c5cf16d7445
babi-techdenali <babi.pepakayala@techdenali.com>
2026-09-17 14:45:59 +0530

    added checkstock method..

--- Store/StoreService.java
+++ Store/StoreService.java
@@ -3,4 +3,7 @@ public class StoreService {
     public void manageStock() {
        System.out.println("Managing store inventory and stock...");
     }
+    public void checkStock() {
+    System.out.println("Checking stock levels");
+    }
 }
