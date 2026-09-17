af695cb4248b5d8a3210f48901703dd08ebdc873
babi-techdenali <babi.pepakayala@techdenali.com>
2026-09-17 16:03:14 +0530

    not deleted..

--- DPR/DprService.java
+++ DPR/DprService.java
@@ -2,5 +2,7 @@ public class DprService {
 
     public void generateReport() {
         System.out.println("Generating DPR report");
+    } public void Report() {
+        System.out.println("Generating DPR........");
     }
 }
