62d2f7a37d5e265afce99b29bf110f10df3b232b
babi-techdenali <babi.pepakayala@techdenali.com>
2026-09-17 15:56:44 +0530

    purchase venodr added

--- Purchase/PurchaseService.java
+++ Purchase/PurchaseService.java
@@ -3,4 +3,7 @@ public class PurchaseService {
     public void createPurchaseRequest() {
         System.out.println("Creating purchase request..");
     }
+    public void selectVendor() {
+    System.out.println("Selecting vendor for purchase request");
+    }
 }
