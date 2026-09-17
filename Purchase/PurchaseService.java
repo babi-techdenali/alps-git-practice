a63ce626e06c8105bcaa6389a0b1fab80bf5814d
babi-techdenali <babi.pepakayala@techdenali.com>
2026-09-17 14:10:17 +0530

    store and purchase updated...

--- Purchase/PurchaseService.java
+++ Purchase/PurchaseService.java
@@ -1,6 +1,6 @@
 public class PurchaseService {
 
     public void createPurchaseRequest() {
-        System.out.println("Creating purchase request");
+        System.out.println("Creating purchase request..");
     }
 }
